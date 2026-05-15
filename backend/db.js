f3aee023e5b4d302a5d3676a77d42ccdd58ae5b1
ianfl <ia.ferreira@duocuc.cl>
2026-05-14 20:51:08 -0400

    Usar DATABASE_URL en Railway

--- backend/db.js
+++ backend/db.js
@@ -6,13 +6,18 @@ if (isLocal) {
   require('dotenv').config();
 }
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: isLocal ? false : { rejectUnauthorized: false },
-});
+const pool = isLocal
+  ? new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: false,
+    })
+  : new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    });
 
 module.exports = pool;
